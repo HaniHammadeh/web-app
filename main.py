f16a9aa9a25f7ca557bd13b832b2e72fc6d9194f
hani.hammadeh@gmail.com <13088805+HaniHammadeh@users.noreply.github.com>
2026-06-20 04:25:13 +0000

    Initial release v1.0

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+    <html>
+    <body style="background-color:blue;color:white;text-align:center;">
+        <h1>BLUE VERSION</h1>
+        <h2>Release v1.0</h2>
+    </body>
+    </html>
+    """
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
